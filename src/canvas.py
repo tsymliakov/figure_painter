d46c671ff309efd797e3d1618bc2b60e917420e2
Denis <makrotheeast@gmail.com>
2024-02-25 13:45:16 +0300

    Definetly the end

--- src/canvas.py
+++ src/canvas.py
@@ -7,23 +7,24 @@ from PIL import Image, ImageDraw, ImageOps
 LOG = logging.getLogger(__name__)
 LOG.addHandler(logging.StreamHandler(sys.stdout))
 
+
 class Canvas:
-    def __init__(self, width: int, height: int, color: str = 'white'):
+    def __init__(self, width: int, height: int):
         self._img = Image.new("RGB", (width, height), "white")
 
     def get_image_draw(self):
         return ImageDraw.Draw(self._img)
 
-    def save(self, path, format='PNG'):
+    def save(self, path, format_='PNG'):
         if not os.path.isdir(path):
             os.makedirs(path)
 
         try:
-            image_path = os.path.join(os.path.abspath(path), f'image.{format.lower()}')
+            image_path = os.path.join(os.path.abspath(path), f'image.{format_.lower()}')
 
             # Я прямо-таки горжусь этим хаком. Координатная сетка в pillow имеет координаты (0, 0)
             # в левом верхнем углу, а не в человеческом левом нижнем
             fl = ImageOps.flip(self._img)
-            fl.save(image_path, format=format)
+            fl.save(image_path, format=format_)
         except PermissionError:
             LOG.error(f'Нет прав для сохранения изображения в директории {path}')
